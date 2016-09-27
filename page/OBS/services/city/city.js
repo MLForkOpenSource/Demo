9e6e55ac2735a2c561ccdb3c95aebfbd60ad872b
zhao365845726 <zhao365845726@163.com>
2016-09-28 00:50:48 +0800

    完善服务的接口
    1.完善天气的调用接口
    2.新增城市列表的精确查找
    3.新增城市列表的模糊查找条件

---
+++ page/OBS/services/city/city.js
@@ -0,0 +1,48 @@
+var initdata = '显示城市信息：\n';
+var cityUrl = [
+    'http://apis.baidu.com/apistore/weatherservice/cityinfo',       //精确查找城市
+    'http://apis.baidu.com/apistore/weatherservice/citylist'        //模糊查找城市
+]
+Page({
+  data: {
+    array:["精确","模糊"],
+    index:0,
+    cityResult: initdata,
+    cityContent:{}         //城市返回的内容
+  },
+  formSubmit: function(e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    console.log(this)
+    var that = this
+    var cityIndex = this.data.index
+    wx.request({
+        url: cityUrl[cityIndex],
+        data: {
+            cityname: e.detail.value.input,
+        },
+        header: {
+            'Content-Type': 'application/json',
+            'apikey':'0379d60b4d898ec65feeb9b13f58943f'
+        },
+        success: function(res) {
+            console.log('res---->')
+            console.log(res.data)
+            that.setData({
+                cityContent : res.data.retData
+            })
+            that.update()
+        }
+    })
+  },
+  formReset: function(e) {
+    console.log('form发生了reset事件，携带数据为：', e.detail.value)
+    this.setData({
+      chosen: ''
+    })
+  },
+  bindPickerChange:function(e){
+    this.setData({
+      index: e.detail.value
+    })
+  },
+})
--- page/OBS/services/weather/weather.js
+++ page/OBS/services/weather/weather.js
@@ -4,9 +4,7 @@ var weatherUrl = [
     'http://apis.baidu.com/apistore/weatherservice/weather',        //根据城市拼音获取天气信息
     'http://apis.baidu.com/apistore/weatherservice/cityname'        //根据城市名称获取天气信息
 ]
-var weatherParamType = [
-    'cityid','cityname','citypinyin'
-]
+
 Page({
   data: {
     array: ['代码', '拼音', '名称'],      //股票类型
@@ -15,47 +13,80 @@ Page({
     weatherContent:{}         //天气信息返回的内容
   },
   formSubmit: function(e) {
-    console.log('form发生了submit事件，携带数据为：', e.detail.value)
-    console.log(this)
+    // console.log('form发生了submit事件，携带数据为：', e.detail.value)
     var that = this
     var weatherIndex = this.data.index
-    wx.request({
-        url: weatherUrl[weatherIndex],
-        data: {
-            //cityid: e.detail.value.input,
-            //cityname: e.detail.value.input,
-            //citypinyin: e.detail.value.input
-        },
-        header: {
-            'Content-Type': 'application/json',
-            'apikey':'0379d60b4d898ec65feeb9b13f58943f'
-        },
-        success: function(res) {
-            console.log('res---->' + res.data)
-            console.log(res.data)
-            that.setData({
-                weatherContent : res.data.retData
-            })
-            that.update()
-        }
-    })
+
+    if(weatherIndex == 0){
+        //根据城市代码来查询天气信息
+        wx.request({
+            url: weatherUrl[weatherIndex],
+            data: {
+                cityid: e.detail.value.input
+            },
+            header: {
+                'Content-Type': 'application/json',
+                'apikey':'0379d60b4d898ec65feeb9b13f58943f'
+            },
+            success: function(res) {
+                console.log(res.data)
+                that.setData({
+                    weatherContent : res.data.retData
+                })
+                that.update()
+            }
+        })
+    }else if(weatherIndex == 1){
+        //根据拼音来查询天气信息
+        wx.request({
+            url: weatherUrl[weatherIndex],
+            data: {
+                citypinyin: e.detail.value.input
+            },
+            header: {
+                'Content-Type': 'application/json',
+                'apikey':'0379d60b4d898ec65feeb9b13f58943f'
+            },
+            success: function(res) {
+                console.log(res.data)
+                that.setData({
+                    weatherContent : res.data.retData
+                })
+                that.update()
+            }
+        })
+    }else if(weatherIndex == 2){
+        //根据名称来查询天气信息
+        wx.request({
+            url: weatherUrl[weatherIndex],
+            data: {
+                cityname: e.detail.value.input
+            },
+            header: {
+                'Content-Type': 'application/json',
+                'apikey':'0379d60b4d898ec65feeb9b13f58943f'
+            },
+            success: function(res) {
+                console.log(res.data)
+                that.setData({
+                    weatherContent : res.data.retData
+                })
+                that.update()
+            }
+        })
+    }
+    
   },
   formReset: function(e) {
-    console.log('form发生了reset事件，携带数据为：', e.detail.value)
+    // console.log('form发生了reset事件，携带数据为：', e.detail.value)
     this.setData({
       chosen: ''
     })
   },
   bindPickerChange:function(e){
-      console.log(e.detail.value)
+    // console.log(e.detail.value)
     this.setData({
       index: e.detail.value
     })
-
-    if(e.detail.value == 0){
-        this.setData({
-            cityid:e.detail.value.input
-        })
-    }
   },
 })
